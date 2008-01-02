6b730b41c84cf50b547db408eab52a450844dd1b
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2008-01-02 23:01:42 +0000

    added test cases

---
+++ trunk/magellan2/test/tc002/481-bsp.cr
@@ -0,0 +1,277 @@
+VERSION 64
+"UTF-8";charset
+"de";locale
+1;noskillpoints
+1196682600;date
+"Eressea";Spiel
+"Java-Tools";Konfiguration
+"Hex";Koordinaten
+36;Basis
+481;Runde
+2;Zeitalter
+"eressea-server@eressea.kn-bremen.de";mailto
+"ERESSEA BEFEHLE";mailcmd
+PARTEI 15289
+"de";locale
+217;age
+6987;Optionen
+296796;Punkte
+223489;Punktedurchschnitt
+"Meermenschen";Typ
+80;Rekrutierungskosten
+1;Anzahl Personen
+"gwyrrd";Magiegebiet
+50;heroes
+50;max_heroes
+"Die Spielkinder";Parteiname
+"foo@example.com";email
+"Wir müssen für alle Experimente herhalten...";banner
+GEGENSTAENDE
+1;Geburtstagstorte
+1;Sonnensegel
+OPTIONEN
+1;AUSWERTUNG
+1;COMPUTER
+0;ZUGVORLAGE
+1;SILBERPOOL
+0;STATISTIK
+0;DEBUG
+1;ZIPPED
+0;ZEITUNG
+1;MATERIALPOOL
+1;ADRESSEN
+0;BZIP2
+1;PUNKTE
+1;SHOWSKCHANGE
+0;XML
+ALLIANZ 696873
+"The Others";Parteiname
+59;Status
+PARTEI 696873
+"The Others";Parteiname
+"others@example.com";email
+"";banner
+"en";locale
+REGION 3 39
+"Osturer";Name
+"Hochland";Terrain
+"neighbour";visibility
+GRENZE 1
+"Feuerwand";typ
+5;richtung
+REGION 2 40
+"Pucul";Name
+"Sumpf";Terrain
+"neighbour";visibility
+REGION 3 38
+"Supinfat";Name
+"Gletscher";Terrain
+"neighbour";visibility
+REGION 2 39
+"Nizir";Name
+"Ebene";Terrain
+"Hier ergießen sich die Flüsse und Bäche der umliegenden Gletscher und Gebirge in den großen Demog. Aus seinem Wasser trinken die Bäume ihren Lebenssaft und gewinnen die Tiere und Zweibeiner neue Kraft.";Beschr
+131;Bauern
+10;Pferde
+3;Silber
+0;Unterh
+3;Rekruten
+10;Lohn
+PREISE
+8;Balsam
+125;Gewürz
+-7;Juwel
+125;Myrrhe
+51;Öl
+132;Seide
+8;Weihrauch
+GRENZE 1
+"Feuerwand";typ
+2;richtung
+GRENZE 2
+"Feuerwand";typ
+4;richtung
+GRENZE 3
+"Feuerwand";typ
+5;richtung
+EINHEIT 1
+"Wonko the Sane";Name
+696873;Partei
+1;Anzahl
+"Menschen";Typ
+GEGENSTAENDE
+1;Silberbeutel
+REGION 1 39
+"Ozean";Terrain
+"travel";visibility
+GRENZE 1
+"Feuerwand";typ
+2;richtung
+DURCHSCHIFFUNG
+"Seewolf (gzu4)"
+REGION 2 38
+"Ozean";Terrain
+"neighbour";visibility
+GRENZE 1
+"Feuerwand";typ
+1;richtung
+REGION 1 40
+"Ozean";Terrain
+"neighbour";visibility
+REGION 14 -26
+"Besbabivud";Name
+"Gletscher";Terrain
+"neighbour";visibility
+REGION 12 -26
+"Dufot";Name
+"Wüste";Terrain
+"neighbour";visibility
+REGION 13 -26
+"Cikafuset";Name
+"Ebene";Terrain
+"neighbour";visibility
+GRENZE 1
+"Straße";typ
+3;richtung
+100;prozent
+REGION 15 -28
+"Ozean";Terrain
+"neighbour";visibility
+REGION 15 -27
+"Vepo";Name
+"Sumpf";Terrain
+"neighbour";visibility
+GRENZE 1
+"Irrlichter";typ
+5;richtung
+REGION 14 -27
+"Falkenfeld";Name
+"Ebene";Terrain
+"Das Falkenfeld ist wahrscheinlich der Grund dafür, dass die Insel den Namen Falkenland, oder auch Falkeninsel bekommen hat. Das fruchtbare Land ist reich ein Feldmäusen und anderen Kleintieren und zieht mit seinen zahlreichen Wehrtürmen entsprechend viele Falken an.";Beschr
+1;Baeume
+1;Schoesslinge
+2746;Bauern
+36;Pferde
+393047;Silber
+19652;Unterh
+68;Rekruten
+13;Lohn
+PREISE
+92;Balsam
+75;Gewürz
+-7;Juwel
+60;Myrrhe
+48;Öl
+102;Seide
+52;Weihrauch
+RESOURCE 1355696724
+"Schößlinge";type
+1;number
+RESOURCE 1923739441
+"Bäume";type
+1;number
+GRENZE 1
+"Straße";typ
+0;richtung
+100;prozent
+GRENZE 2
+"Irrlichter";typ
+2;richtung
+GRENZE 3
+"Straße";typ
+4;richtung
+100;prozent
+DURCHSCHIFFUNG
+"Idefix (w5e4)"
+"Tarantel (8k4c)"
+DURCHREISE
+"Xedru (31hs)"
+MESSAGE 358196680
+798169913;type
+"In Falkenfeld (14,-27) erhoben sich die Toten aus den Gräbern.";rendered
+14 -27 0;region
+MESSAGE 398972488
+2122087327;type
+"40 Bauern flohen aus Furcht vor Erschlagene der Verfluchten (Lnme).";rendered
+40;amount
+1010390;unit
+MESSAGE 398972536
+1493414654;type
+"Erschlagene der Verfluchten (Lnme) verspeiste 21 Bauern.";rendered
+1010390;unit
+21;amount
+BURG 503246
+"Burg";Typ
+"Falkentürme";Name
+"";Beschr
+374;Groesse
+1539317;Besitzer
+15289;Partei
+EINHEIT 1539317
+"Falke";Name
+"";Beschr
+15289;Partei
+"Mond";typprefix
+9;Anzahl
+"Halblinge";Typ
+503246;Burg
+1;bewacht
+GEGENSTAENDE
+9;Schild
+9;Schwert
+EINHEIT 1010390
+"Erschlagene der Verfluchten";Name
+1;Parteitarnung
+779;Anzahl
+"Zombies";Typ
+GEGENSTAENDE
+779;Schartiges Schwert
+REGION 12 -27
+"Ozean";Terrain
+"neighbour";visibility
+REGION 13 -27
+"Ozean";Terrain
+"travel";visibility
+GRENZE 1
+"Irrlichter";typ
+4;richtung
+DURCHSCHIFFUNG
+"Idefix (w5e4)"
+"Tarantel (8k4c)"
+SCHIFF 1184248
+"Seevogel";Name
+"";Beschr
+"Langboot";Typ
+50;Groesse
+2;Schaden
+1086490;Kapitaen
+696873;Partei
+EINHEIT 1086490
+"Irvin";Name
+696873;Partei
+1;Anzahl
+"Meermenschen";Typ
+1184248;Schiff
+REGION 13 -28
+"Ozean";Terrain
+"neighbour";visibility
+GRENZE 1
+"Irrlichter";typ
+1;richtung
+REGION 14 -28
+"Wyrmtiefen";Name
+"Ebene";Terrain
+"neighbour";visibility
+GRENZE 1
+"Straße";typ
+1;richtung
+100;prozent
+MESSAGETYPE 798169913
+"\"In $region($region) erhoben sich die Toten aus den Gräbern.\"";text
+"events";section
+MESSAGETYPE 2122087327
+"\"$int($amount) Bauern flohen aus Furcht vor $unit($unit).\"";text
+"events";section
+MESSAGETYPE 1493414654
+"\"$unit($unit) verspeiste $int($amount) Bauern.\"";text
+"events";section

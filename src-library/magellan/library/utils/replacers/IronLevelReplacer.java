5dd5ecf3f3ffe5750e42bb18224b3a82f9e9d6c5
Fiete <41542859+FieteFietz@users.noreply.github.com>
2008-01-16 12:39:49 +0000

    added some replacers. fiexd tooltipinfo

---
+++ src-library/magellan/library/utils/replacers/IronLevelReplacer.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2000-2004 Roger Butenuth, Andreas Gampe,
+ *                          Stefan Goetz, Sebastian Pappert,
+ *                          Klaas Prause, Enno Rehling,
+ *                          Sebastian Tusk, Ulrich Kuester,
+ *                          Ilja Pavkovic
+ *
+ * This file is part of the Eressea Java Code Base, see the
+ * file LICENSING for the licensing information applying to
+ * this file.
+ *
+ */
+
+package magellan.library.utils.replacers;
+
+import magellan.library.Region;
+import magellan.library.RegionResource;
+import magellan.library.rules.ItemType;
+import magellan.library.utils.Resources;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Fiete
+ * @version 1.0
+ */
+public class IronLevelReplacer extends AbstractRegionReplacer {
+	/**
+	 * DOCUMENT-ME
+	 *
+	 * 
+	 *
+	 * 
+	 */
+	public Object getRegionReplacement(Region region) {
+		ItemType ironType = region.getData().rules.getItemType("Eisen");
+		if (ironType==null){
+			return null;
+		}
+		RegionResource ironResource = region.getResource(ironType);
+		if (ironResource==null){
+			return null;
+		}
+		return new Integer(ironResource.getSkillLevel());
+	}
+
+  public String getDescription() {
+    return Resources.get("util.replacers.ironlevelreplacer.description");
+  }
+}

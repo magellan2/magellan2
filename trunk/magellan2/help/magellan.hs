a1444197116ecb7e0cea3975259c65e2b81157b6
trickert <trickert@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2008-03-01 11:42:52 +0000

    Moved Help files to subdirectory for a better overview...

--- trunk/magellan2/help/magellan.hs
+++ trunk/magellan2/help/magellan.hs
@@ -1,11 +1,9 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
-<!DOCTYPE helpset
-  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
-         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
 <helpset version="1.0" xml:lang="de">
 
   <!-- title -->
-  <title>Magellan Online-Hilfe</title>
+  <title>Magellan Hilfe</title>
 
   <!-- maps -->
   <maps>

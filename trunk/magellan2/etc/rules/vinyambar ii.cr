f6a33acbd80ba628d6cad7c03965f4778458ebac
fiete <fiete@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2008-07-30 14:37:24 +0000

    Schlangenkopf->Seeschlangenkopf

--- trunk/magellan2/etc/rules/vinyambar ii.cr
+++ trunk/magellan2/etc/rules/vinyambar ii.cr
@@ -1356,8 +1356,8 @@ ITEM "Magischer Kräuterbeutel"
 "misc";category
 1;storeinbonw
 
-ITEM "Schlangenkopf"
-"Schlangenkopf";name
+ITEM "Seeschlangenkopf"
+"Seeschlangenkopf";name
 5;weight
 "trophies";category
 1;storeinbonw

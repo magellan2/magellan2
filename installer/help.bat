45eac3e16e535d07955fc036a4cd482764b49864
Thoralf Rickert-Wendt <30341294+trickert76@users.noreply.github.com>
2009-04-22 07:37:27 +0000

    fix for start under windows if JAVA_HOME is set

--- installer/help.bat
+++ installer/help.bat
@@ -1,2 +1,9 @@
 @echo off
-start javaw -jar magellan-client.jar --help
+
+if not exist "%JAVA_HOME%\bin\javaw.exe" goto noJavaHome
+start "Magellan" "%JAVA_HOME%\bin\javaw.exe" -Xmx400m -Xms400m -jar "magellan-client.jar" --help
+goto eof
+
+:noJavaHome
+start javaw -Xmx400m -Xms400m -jar "magellan-client.jar" --help
+goto eof
--- installer/magellan.bat
+++ installer/magellan.bat
@@ -1,6 +1,6 @@
 @echo off
 if not exist "%JAVA_HOME%\bin\javaw.exe" goto noJavaHome
-start "%JAVA_HOME%\bin\javaw.exe" -Xmx400m -Xms400m -jar "magellan-client.jar" %1
+start "Magellan" "%JAVA_HOME%\bin\javaw.exe" -Xmx400m -Xms400m -jar "magellan-client.jar" %1
 goto eof
 
 :noJavaHome

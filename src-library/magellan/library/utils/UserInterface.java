bc6e1f19d231062016f8ec31058fdc3082faa236
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2009-07-20 23:15:39 +0000

    comments

--- src-library/magellan/library/utils/UserInterface.java
+++ src-library/magellan/library/utils/UserInterface.java
@@ -13,22 +13,21 @@
 // 
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
 // along with this program (see doc/LICENCE.txt); if not, write to the
-// Free Software Foundation, Inc., 
-// 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// Free Software Foundation, Inc.,
+// 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 // 
 package magellan.library.utils;
 
 import javax.swing.JDialog;
 
 /**
- * This interface represents a UserInterface that shows the user
- * the progress of an action.
- *
+ * This interface represents a UserInterface that shows the user the progress of an action.
+ * 
  * @author unknown (extracted from ReportMerger)
  * @author Thoralf Rickert
  * @version 1.0, 07.11.2007
@@ -36,50 +35,52 @@ import javax.swing.JDialog;
 public interface UserInterface {
 
   /**
-   * Shows the dialog
-   */ 
+   * Shows the dialog. If the action is done in a different thread, this method should be called in
+   * the calling thread (usually the EventDispatchThread).
+   */
   public void show();
-  
+
   /**
-   * Notifies the interface that the task is done. Destroys progress dialog.
+   * Notifies the interface that the task is done. Destroys progress dialog. If the action is done
+   * in a different thread, this method should be called in the working thread.
    */
   public void ready();
-  
+
   /**
    * Sets the progress of the action.
    */
   public void setProgress(String strMessage, int iProgress);
-  
+
   /**
-   * Sets the maximum number of steps.
+   * Sets the maximum number of steps. A value of 0 or less means, that the maximum is
+   * indeterminate.
    */
   public void setMaximum(int maxProgress);
-  
+
   /**
    * Sets the title of the progress dialog.
    */
   public void setTitle(String title);
 
-  
   /**
    * Displays an error window
    * 
    * @param message A message for the user, may be <code>null</code>.
    * @param description An explanatory text, may be <code>null</code>
-   * @param exception  
+   * @param exception
    */
   public void showException(String message, String description, Exception exception);
-  
+
   /**
    * Opens a confirm dialog.
    */
   public boolean confirm(String strMessage, String strTitle);
-  
+
   /**
    * Opens an input dialog.
    */
   public Object input(String strMessage, String strTitle, Object[] values, Object initial);
-  
+
   /**
    * Shows a message with an ok option
    * 
@@ -87,12 +88,11 @@ public interface UserInterface {
    */
   public void showMessageDialog(String message);
 
-  
   /**
    * Displays the dialog.
    * 
    * @param dialog
    */
   public void showDialog(JDialog dialog);
-  
+
 }

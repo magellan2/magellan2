a9394026198b4cfc3710fb44f51af8fbb9fddaec
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2010-02-11 20:16:15 +0000

    removed Xms

--- trunk/magellan2/installer/magellan.bat
+++ trunk/magellan2/installer/magellan.bat
@@ -1,10 +1,10 @@
 @echo off
 if not exist "%JAVA_HOME%\bin\javaw.exe" goto noJavaHome
-start "Magellan" "%JAVA_HOME%\bin\javaw.exe" -Xmx400m -Xms400m -jar "magellan-client.jar" %1
+start "Magellan" "%JAVA_HOME%\bin\javaw.exe" -Xmx400m -jar "magellan-client.jar" %1
 goto eof
 
 :noJavaHome
-start javaw -Xmx400m -Xms400m -jar "magellan-client.jar" %1
+start javaw -Xmx400m -jar "magellan-client.jar" %1
 goto eof
 
 

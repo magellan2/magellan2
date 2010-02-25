6ba24ef353631416ee8f2f695871024d55b61148
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2010-02-25 15:09:04 +0000

    Undefined values are evaluated as <code>&gt; 0</code>.

--- src-client/magellan/client/swing/ProgressBarUI.java
+++ src-client/magellan/client/swing/ProgressBarUI.java
@@ -352,6 +352,7 @@ public class ProgressBarUI implements UserInterface, ActionListener {
       }
 
       initComponents();
+      super.setFocusableWindowState(false);
       setLocationRelativeTo(getParent());
       pack();
     }

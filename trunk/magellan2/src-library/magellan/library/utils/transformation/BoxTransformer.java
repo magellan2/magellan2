916fc333ae8589e60570b4fca3446d29c4248cf0
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2010-02-28 18:45:29 +0000

    more convenience methods

--- trunk/magellan2/src-library/magellan/library/utils/transformation/BoxTransformer.java
+++ trunk/magellan2/src-library/magellan/library/utils/transformation/BoxTransformer.java
@@ -9,7 +9,14 @@ public class BoxTransformer implements ReportTransformer {
 
   private MapTransformer.BBoxes boxes;
 
+  /**
+   * Creates a new Transformer the puts all coordinates into the boxes.
+   *
+   * @throws NullPointerException if boxes is <code>null</code>
+   */
   public BoxTransformer(MapTransformer.BBoxes boxes) {
+    if (boxes == null)
+      throw new NullPointerException();
     this.boxes = boxes;
   }
 

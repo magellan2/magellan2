1c17ace505f7235be0e84f0caa80794d76da32ee
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2012-02-08 23:25:35 +0000

    fixed invalid date glitch

--- src-library/magellan/library/rules/EresseaDate.java
+++ src-library/magellan/library/rules/EresseaDate.java
@@ -7,9 +7,15 @@
 
 package magellan.library.rules;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.Random;
 
 import magellan.library.utils.Resources;
+import magellan.library.utils.SelfCleaningProperties;
 import magellan.library.utils.logging.Logger;
 
 /**
@@ -20,9 +26,9 @@ import magellan.library.utils.logging.Logger;
 public class EresseaDate extends Date {
   private static final Logger log = Logger.getInstance(EresseaDate.class);
   private static final String months_old[] = { "Januar", "Februar", "März", "April", "Mai", "Juni",
-      "Juli", "August", "September", "Oktober", "November", "Dezember" };
+    "Juli", "August", "September", "Oktober", "November", "Dezember" };
   private static final String months_new[] = { "Feldsegen", "Nebeltage", "Sturmmond", "Herdfeuer",
-      "Eiswind", "Schneebann", "Blütenregen", "Mond_der_milden_Winde", "Sonnenfeuer" };
+    "Eiswind", "Schneebann", "Blütenregen", "Mond_der_milden_Winde", "Sonnenfeuer" };
 
   private static final int EPOCH1_START = 0;
   private static final int EPOCH2_START = 184;
@@ -108,80 +114,85 @@ public class EresseaDate extends Date {
       String strDate = "";
 
       if (getEpoch() == 1) {
-        // first age
-        switch (iDateType) {
-        default:
-        case TYPE_SHORT:
-          strDate = EresseaDate.months_old[(iDate - 1) % 12] + " " + (((iDate - 1) / 12) + 1);
+        if (iDate<1) {
+          strDate = Resources.get("rules.eresseadate.invalid");
+        } else {
+          // first age
+          switch (iDateType) {
+          default:
+          case TYPE_SHORT:
+            strDate = EresseaDate.months_old[(iDate - 1) % 12] + " " + (((iDate - 1) / 12) + 1);
+            break;
 
-          break;
+          case TYPE_LONG:
+            strDate =
+            Resources.get("rules.eresseadate.epoch1.type_long",
+                EresseaDate.months_old[(iDate - 1) % 12], (((iDate - 1) / 12) + 1));
+            break;
 
-        case TYPE_LONG:
-          strDate =
-              EresseaDate.months_old[(iDate - 1) % 12] + " des Jahres " + (((iDate - 1) / 12) + 1)
-                  + " im ersten Zeitalter";
-
-          break;
-
-        case TYPE_PHRASE:
-          strDate =
-              "Wir schreiben den " + EresseaDate.months_old[(iDate - 1) % 12] + " des Jahres "
-                  + (((iDate - 1) / 12) + 1) + " im ersten Zeitalter.";
-
-          break;
+          case TYPE_PHRASE:
+            strDate =
+            Resources.get("rules.eresseadate.epoch1.type_long",
+                EresseaDate.months_old[(iDate - 1) % 12], (((iDate - 1) / 12) + 1));
+            break;
+          }
         }
       } else if (getEpoch() >= 2) {
         // second and third age
         int iDate2 = getWeekFromStart();
+        if (iDate2<0) {
+          strDate = Resources.get("rules.eresseadate.invalid");
+        } else {
 
-        int iWeek = iDate2 % 3;
-        String strWeek = Resources.get("rules.eresseadate.week_short." + (iWeek + 1));
-        String strMonth =
-            Resources.get("rules.eresseadate." + EresseaDate.months_new[(iDate2 / 3) % 9]);
-        int iYear = (iDate2 / 27) + 1;
+          int iWeek = iDate2 % 3;
+          String strWeek = Resources.get("rules.eresseadate.week_short." + (iWeek + 1));
+          String strMonth =
+              Resources.get("rules.eresseadate." + EresseaDate.months_new[(iDate2 / 3) % 9]);
+          int iYear = (iDate2 / 27) + 1;
 
-        switch (iDateType) {
-        default:
-        case Date.TYPE_SHORT: {
-          strDate = Resources.get("rules.eresseadate.type_short", strWeek, strMonth, iYear);
-        }
+          switch (iDateType) {
+          default:
+          case Date.TYPE_SHORT: {
+            strDate = Resources.get("rules.eresseadate.type_short", strWeek, strMonth, iYear);
+          }
           break;
 
-        case Date.TYPE_LONG: {
-          // select one of three phrases at random
-          int random = r.nextInt(3);
-          String strWeekLong =
-              Resources.get("rules.eresseadate.week_long." + (iWeek + 1) + "." + random);
-          String strAge = Resources.get("rules.eresseadate.age_long." + getEpoch());
-          strDate =
-              Resources.get("rules.eresseadate.type_long." + random, strWeekLong, strMonth, iYear,
-                  strAge);
-        }
+          case Date.TYPE_LONG: {
+            // select one of three phrases at random
+            int random = r.nextInt(3);
+            String strWeekLong =
+                Resources.get("rules.eresseadate.week_long." + (iWeek + 1) + "." + random);
+            String strAge = Resources.get("rules.eresseadate.age_long." + getEpoch());
+            strDate =
+                Resources.get("rules.eresseadate.type_long." + random, strWeekLong, strMonth, iYear,
+                    strAge);
+          }
           break;
 
-        case TYPE_PHRASE: {
-          int random = r.nextInt(3);
-          String strWeekLong =
-              Resources.get("rules.eresseadate.week_phrase." + (iWeek + 1) + "." + random);
-          String strAge = Resources.get("rules.eresseadate.age_phrase." + getEpoch());
-          strDate =
-              Resources.get("rules.eresseadate.type_phrase." + random, strWeekLong, strMonth,
-                  iYear, strAge);
-        }
+          case TYPE_PHRASE: {
+            int random = r.nextInt(3);
+            String strWeekLong =
+                Resources.get("rules.eresseadate.week_phrase." + (iWeek + 1) + "." + random);
+            String strAge = Resources.get("rules.eresseadate.age_phrase." + getEpoch());
+            strDate =
+                Resources.get("rules.eresseadate.type_phrase." + random, strWeekLong, strMonth,
+                    iYear, strAge);
+          }
           break;
 
-        case TYPE_PHRASE_AND_SEASON: {
-          int random = r.nextInt(3);
+          case TYPE_PHRASE_AND_SEASON: {
+            int random = r.nextInt(3);
 
-          String strWeekLong =
-              Resources.get("rules.eresseadate.week_phrase." + (iWeek + 1) + "." + random);
-          String strAge = Resources.get("rules.eresseadate.age_phrase." + getEpoch());
-          String strSeason = " " + Resources.get("rules.eresseadate.season_phrase." + getSeason());
-          strDate =
-              Resources.get("rules.eresseadate.type_phrase_season." + random, strWeekLong,
-                  strMonth, iYear, strAge, strSeason);
-        }
+            String strWeekLong =
+                Resources.get("rules.eresseadate.week_phrase." + (iWeek + 1) + "." + random);
+            String strAge = Resources.get("rules.eresseadate.age_phrase." + getEpoch());
+            String strSeason = " " + Resources.get("rules.eresseadate.season_phrase." + getSeason());
+            strDate =
+                Resources.get("rules.eresseadate.type_phrase_season." + random, strWeekLong,
+                    strMonth, iYear, strAge, strSeason);
+          }
           break;
+          }
         }
       }
       strDate = strDate.replaceAll("  ", " ");
@@ -258,4 +269,64 @@ public class EresseaDate extends Date {
     }
   }
 
+  private static Properties loadSettings(File directory, String fileName) {
+    Properties settings = new SelfCleaningProperties();
+    // settings = new OrderedOutputProperties();
+    // settings = new AgingProperties();
+
+    settings.clear();
+
+    File settingsFile = new File(directory, fileName);
+
+    // load settings from file
+    if (settingsFile.exists()) {
+      try {
+        BufferedInputStream stream = null;
+        try {
+          settings.load(stream = new BufferedInputStream(new FileInputStream(settingsFile)));
+          log.info("successfully loaded " + settingsFile);
+        } finally {
+          if (stream != null) {
+            stream.close();
+          }
+        }
+      } catch (IOException e) {
+        log.error("Error while loading " + settingsFile, e);
+        return null;
+      }
+    } else
+      return null;
+    return settings;
+  }
+
+  /**
+   * Test method.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    Resources.initStaticPaths(loadSettings(new File(".", "sandbox/de"), "magellan.ini"));
+    Resources.getInstance().initialize(new File("."), "");
+
+    for (int i=1; i<24; ++i) {
+      EresseaDate date = new EresseaDate(i);
+      System.out.println(date.toString(TYPE_SHORT));
+      System.out.println(date.toString(TYPE_LONG));
+      System.out.println(date.toString(TYPE_PHRASE));
+      System.out.println(date.toString(TYPE_PHRASE_AND_SEASON));
+    }
+    System.out.println();
+    EresseaDate date = new EresseaDate(1);
+    date.setEpoch(2);
+    System.out.println(date.toString(TYPE_SHORT));
+    System.out.println();
+    for (int i=1; i<50; ++i) {
+      date = new EresseaDate(182+i);
+      date.setEpoch(2);
+      System.out.println(date.toString(TYPE_SHORT));
+      System.out.println(date.toString(TYPE_LONG));
+      System.out.println(date.toString(TYPE_PHRASE));
+      System.out.println(date.toString(TYPE_PHRASE_AND_SEASON));
+    }
+  }
 }

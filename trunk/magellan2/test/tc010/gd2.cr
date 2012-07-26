adbdcdcbe59cb8020a8fd2fd50c8af434cd20bd3
stm_ <stm_@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2012-07-26 12:53:55 +0000

    amend to previous commit

---
+++ trunk/magellan2/test/tc010/gd2.cr
@@ -0,0 +1,63 @@
+VERSION 66
+"ISO-8859-1";charset
+"de";locale
+1;noskillpoints
+1343300321;date
+"Eressea";Spiel
+"Java-Tools";Konfiguration
+"Hex";Koordinaten
+36;Basis
+1;Umlaute
+351;Runde
+2;Zeitalter
+867718;reportowner
+PARTEI 867718
+"Meermenschen";Typ
+80;Rekrutierungskosten
+"Faction_867718";Parteiname
+ISLAND 1
+"Island_1";name
+REGION 0 0
+"Region_0_0";Name
+"Gletscher";Terrain
+"1";Insel
+BURG 397
+"Burg";Typ
+"Burg 1";Name
+10;Groesse
+45397;Besitzer
+867718;Partei
+BURG 398
+"Burg";Typ
+"Burg 2";Name
+10;Groesse
+42806;Besitzer
+867718;Partei
+EINHEIT 45397
+"1_1";Name
+867718;Partei
+1;Anzahl
+"Meermenschen";Typ
+"Meermenschen";wahrerTyp
+397;Burg
+COMMANDS
+""
+EINHEIT 44102
+"1_2";Name
+867718;Partei
+1;Anzahl
+"Meermenschen";Typ
+"Meermenschen";wahrerTyp
+397;Burg
+COMMANDS
+""
+EINHEIT 42806
+"1_3";Name
+867718;Partei
+1;Anzahl
+"Meermenschen";Typ
+"Meermenschen";wahrerTyp
+398;Burg
+COMMANDS
+""
+TRANSLATION

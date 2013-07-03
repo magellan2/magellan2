bb965f7f6b3adce5bbfbc2252fd0308d0c7d7dcd
stm2 <stm2@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2013-07-03 17:16:28 +0000

    atlantis build 6

--- trunk/magellan2/src-library/magellan/library/GameData.java
+++ trunk/magellan2/src-library/magellan/library/GameData.java
@@ -93,7 +93,7 @@ public abstract class GameData implements Cloneable, Addeable {
    * @deprecated use {@link #getRules()}
    */
   @Deprecated
-  private final Rules rules;
+  public final Rules rules;
 
   private final MapMetric mapMetric;
 

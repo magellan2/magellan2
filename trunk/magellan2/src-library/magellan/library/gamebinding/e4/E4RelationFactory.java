55b1e321bf61f55eb655c1b6bd5811d8f058dbd7
stm2 <stm2@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2014-05-04 23:42:41 +0000

    added Deveron support

---
+++ trunk/magellan2/src-library/magellan/library/gamebinding/e4/E4RelationFactory.java
@@ -0,0 +1,26 @@
+/*
+ *  Copyright (C) 2000-2004 Roger Butenuth, Andreas Gampe,
+ *                          Stefan Goetz, Sebastian Pappert,
+ *                          Klaas Prause, Enno Rehling,
+ *                          Sebastian Tusk, Ulrich Kuester,
+ *                          Ilja Pavkovic
+ *
+ * This file is part of the Eressea Java Code Base, see the
+ * file LICENSING for the licensing information applying to
+ * this file.
+ *
+ */
+
+package magellan.library.gamebinding.e4;
+
+import magellan.library.Rules;
+import magellan.library.gamebinding.e3a.E3ARelationFactory;
+
+/**
+ */
+public class E4RelationFactory extends E3ARelationFactory {
+  protected E4RelationFactory(Rules rules) {
+    super(rules);
+  }
+
+}

0d9a00a36f874672519d97ca3a1a275f1864a012
stm2 <stm2@1c5abfc5-b992-4a1f-8ccd-9ce068e22774>
2015-01-08 20:36:41 +0000

    FOLLOW SHIP is short(ish) order now

--- trunk/magellan2/src-test/magellan/library/gamebinding/EresseaOrderChangerTest.java
+++ trunk/magellan2/src-test/magellan/library/gamebinding/EresseaOrderChangerTest.java
@@ -10,17 +10,17 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program (see doc/LICENCE.txt); if not, write to the
-// Free Software Foundation, Inc., 
+// Free Software Foundation, Inc.,
 // 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-// 
+//
 package magellan.library.gamebinding;
 
 import static org.junit.Assert.assertEquals;
@@ -43,7 +43,7 @@ import org.junit.Test;
 
 /**
  * Test for EresseaOrderChanger
- * 
+ *
  * @author stm
  */
 public class EresseaOrderChangerTest extends MagellanTestWithResources {
@@ -62,6 +62,22 @@ public class EresseaOrderChangerTest extends MagellanTestWithResources {
     data = builder.createSimpleGameData();
     unit = data.getUnits().iterator().next();
     changer = (EresseaOrderChanger) data.getGameSpecificStuff().getOrderChanger();
+  }
+
+  @Test
+  public final void testAreCompatibleLongOrdersFollow() {
+    unit.addOrder("FOLGE EINHEIT abc");
+    unit.addOrder("ARBEITE");
+    assertEquals(-1, changer.areCompatibleLongOrders(unit.getOrders2()));
+
+    unit.clearOrders();
+    unit.addOrder("FOLGE SCHIFF abc");
+    unit.addOrder("ARBEITE");
+    assertEquals(-1, changer.areCompatibleLongOrders(unit.getOrders2()));
+
+    unit.addOrder("FOLGE EINHEIT abc");
+    unit.addOrder("FOLGE EINHEIT bcd");
+    assertEquals(-1, changer.areCompatibleLongOrders(unit.getOrders2()));
   }
 
   /**

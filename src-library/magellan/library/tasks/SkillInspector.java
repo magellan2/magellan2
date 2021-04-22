1db9c7defa02b7bbf5577ff74fe960bdbe3d1885
Steffen Mecke <stm2@users.sf.net>
2021-04-22 11:17:05 +0200

    fixed issue with SkillInspector and temp units

--- src-library/magellan/library/tasks/SkillInspector.java
+++ src-library/magellan/library/tasks/SkillInspector.java
@@ -10,17 +10,17 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program (see doc/LICENCE.txt); if not, write to the
 // Free Software Foundation, Inc.,
 // 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-// 
+//
 package magellan.library.tasks;
 
 import java.util.ArrayList;
@@ -69,7 +69,7 @@ public class SkillInspector extends AbstractInspector {
       Unit u2 = relation.target;
       if (u1 != u || (u2 instanceof ZeroUnit)
           || !(u2.getPersons() > 0 || u2.getRelations(PersonTransferRelation.class).size() > 1)) {
-        break;
+        continue;
       }
       List<Skill> skills = new LinkedList<Skill>(u1.getModifiedSkills());
       skills.addAll(u2.getModifiedSkills());
